ea48f3d4d60997b704c29d36658fa8571093e9d8
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-02-07 13:28:43 +0300

    User Registration and Login Authentication

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('update_item/', views.updateItem, name='update_item'),
     path('process_order/', views.processOrder, name='process_order'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout')
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 import json
 import datetime
 from .models import *
 from .utils import *
+from .forms import *
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            messages.info(request, 'User or password is incorrect')
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def store(request):
